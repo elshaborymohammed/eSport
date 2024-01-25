2dcad3583cc5901b876c4d1c38028976d348d9b3
malshaboury.eg <malshaboury.eg@solutions.com.sa>
2024-01-25 17:23:36 +0200

    commit

--- db/delete_contract_data.sql
+++
@@ -1,7 +0,0 @@
-DELETE
-FROM `team-club-qa`.contract_history
-WHERE contract_history.member_contract_id = :?1;
-
-DELETE
-FROM `team-club-qa`.member_contract
-WHERE id = :?1;
--- script/init/03-init-apisix.sh
+++
@@ -1,5 +0,0 @@
-sh 04-init-apisix-upstreams.sh
-printf "\n\n"
-
-sh 05-init-apisix-routes.sh
-printf "\n\n"
